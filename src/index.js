0955aff1306f5305f2319ffa34418095856a475f
Youngwoo <zestlee1106@gmail.com>
2023-02-04 20:09:19 +0900

    strictMode 삭제
    - strictMode 는 create react 로 리액트 프로젝트를 만들면 저절로 감싸져서 나온다
    - 해당 태그가 있을 경우 그릴 때 자손까지 검사하기 때문에 렌더가 두 번 실행된다

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
+//아래처럼 StrictMode 로 감싸져있을 경우 자손까지 검사하기 때문에 기본적으로 렌더링이 두 번 실행된다
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
