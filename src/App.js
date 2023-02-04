6896e7c24eb2dbc20c2277cd4d91cbd82930abd3
Youngwoo <zestlee1106@gmail.com>
2023-02-04 20:51:01 +0900

    useEffect 추가
    - component 내부에 있는 코드들은 렌더링이 될 때 항상 실행된다
    - state 가 바뀔 때마다 컴포넌트는 렌더링이 된다
    - 그 말은 즉슨, state 가 바뀔 때마다 component 의 내부에 있는 코드들은 항상 실행이 된다
    - 예를 들어 API 를 호출하는 로직이 내부에 있다면, 어느 state 가 바뀔 때마다 항상 실행되기 때문에, 필요없는 리소스를 잡아먹을 수 있다.
    - 그럴 일을 덜기 위해서 useEffect 를 사용히는 것이다.
    - 두번쨰 인자에 텅 빈 array 를 넣으면 초기 렌더 시점에만 실행이 되고, 특정 state 를 넣으면 해당 state 가 바뀔 때에만 실행이 된다.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("render");
+  const onChange = (event) => setKeyword(event.target.value);
+
+  useEffect(() => {
+    console.log("I run only once");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword' & 'counter' changes.");
+  }, [keyword, counter]);
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here"
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
